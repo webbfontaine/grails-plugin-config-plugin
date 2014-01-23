c7da16699edd093d93f24409c3eb5d38b82f26d3
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2014-01-23 09:23:11 +0100

    Clean up - removed useless comments.

--- PluginConfigGrailsPlugin.groovy
+++ PluginConfigGrailsPlugin.groovy
@@ -17,11 +17,8 @@ import org.grails.plugin.config.DefaultConfigHelper
 
 class PluginConfigGrailsPlugin {
 
-    // the plugin version
     def version = '0.2.0'
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = '2.0 > *'
-    // the other plugins this plugin depends on
     def dependsOn = ['core': '* > 1.0']
 
     def loadBefore = ['logging']
